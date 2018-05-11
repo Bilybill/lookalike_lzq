dfaf229bb9002bee7ea7b03e0cb0dea596620a1a
lzq <louzq16@mails.tsinghua.edu.cn>
2018-05-11 22:13:07 +0800

    encoder_user_12_lzq

--- data/data_fenxi.py
+++ data/data_fenxi.py
@@ -2,7 +2,7 @@ import pickle
 import numpy as np
 
 f_inte_path=r'alluserinterest.txt'
-
+f_in_fenxi_path=r'interest_number_fenxi'
 def mod(x):
     if not isinstance(x,np.ndarray):
         raise TypeError('bad operand type')
@@ -28,8 +28,12 @@ def interest():
         in_fenxi_dict['mode']=mod(np_a)
     return in_fenxi_dict
 
+def load(fpath=f_in_fenxi_path):
+    with open(fpath,'rb') as f:
+        data=pickle.load(f)
+    return data
 #a=interest()
 #f=open("interest_number_fenxi",'wb')
 #pickle.dump(a,f)
 #f.close()
-print(a)
+#print(a)
